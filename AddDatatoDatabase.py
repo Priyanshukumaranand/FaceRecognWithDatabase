ac5bc8ae43213fc1d5bdb5c86bf84dd66f3d1062
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-07-09 21:36:45 +0530

    first

---
+++ AddDatatoDatabase.py
@@ -0,0 +1,36 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
+
+ref = db.reference('Students')
+
+data = {
+    "321654":
+        {
+            "name": "Priyanshu",
+            "major": "Computer",
+            "starting_year": 2023,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
